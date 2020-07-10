ebe61bb7508e6aa09257b9f7efc16c5892b52afe
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-10 18:44:01 +0530

    Added program to calculate percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -x
-result=$((RANDOM%2))
-if [ $result = 0 ]
-then
-        echo HEADS
-else
-        echo TAILS
-fi
+declare -A singletDictionary
+read -p "Enter how many times you want to flip coin : " noOfFlips
+headCount=0
+tailCount=0
+for((i=0;i<$noOfFlips;i++))
+do
+        result=$((RANDOM%2))
+        if [ $result = 0 ]
+        then
+                ((headCount++))
+                echo HEADS
+        else
+                ((tailCount++))
+                echo TAILS
+        fi
+done
+singletDictionary[H]="$headCount"
+singletDictionary[T]="$tailCount"
+headPercentage=`expr ${singletDictionary[H]}/$noOfFlips | bc -l`
+tailPercentage=`expr ${singletDictionary[T]}/$noOfFlips | bc -l`
